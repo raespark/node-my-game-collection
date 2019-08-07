bafee0b09208a5b7797756d0c1467dd4280d4f06
Rachael Metcalf <raespark.games@gmail.com>
2019-08-07 00:49:45 -0500

    add home page component

--- src/App.js
+++ src/App.js
@@ -1,89 +1,12 @@
 import React, { Component } from 'react';
-import NavBar from './Components/NavBar/NavBar';
-import FilterSideBar from './Components/FilterSidebar/FilterSidebar';
-import GameList from './Components/GameList/GameList';
-import GameData from './mockGameData';
+import HomePage from './Components/HomePage/HomePage';
 import './App.less';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      games: GameData,
-      genreFilters: [],
-      platformFilters: [],
-      ratingFilter: 0,
-      textFilter: ''
-    }
-
-    this.filter = this.filter.bind(this);
-    this.handleToggleFilter = this.handleToggleFilter.bind(this);
-  }
-
-  filter() {
-    let games = GameData;
-    games = games.filter((game) => {
-      let genreFilter = true;
-      let platformFilter = true;
-      let ratingFilter = true;
-      let textFilter = true;
-
-      this.state.genreFilters.forEach((genreId) => {
-        genreFilter = genreFilter && game.genres.some(genre => genre.id === genreId);
-      })
-
-      this.state.platformFilters.forEach((platformId) => {
-        platformFilter = platformFilter && game.platforms.some(platform => platform.id === platformId);
-      })
-
-      ratingFilter = game.rating >= this.state.ratingFilter;
-
-      textFilter = game.title.toLowerCase().includes(this.state.textFilter.toLowerCase());
-
-      return genreFilter && platformFilter && ratingFilter && textFilter;
-    });
-
-    this.setState({games});
-  }
-
-  addOrRemoveFilter(id, filterList) {
-    let activeFilters = filterList;
-    let activeIndex = activeFilters.indexOf(id);
-
-    if(activeIndex > -1){
-      activeFilters.splice(activeIndex, 1);
-    }
-    else {
-      activeFilters.push(id);
-    }
-
-    return activeFilters;
-  }
-
-  handleToggleFilter(filter, type) {
-    if(type === 0){
-      this.setState({genreFilters: this.addOrRemoveFilter(filter, this.state.genreFilters)}, this.filter);
-    }
-    if(type === 1){
-      this.setState({platformFilters: this.addOrRemoveFilter(filter, this.state.platformFilters)}, this.filter);
-    }
-    if(type === 2) {
-      this.setState({ratingFilter: filter}, this.filter);
-    }
-
-    if(type === 3) {
-      this.setState({textFilter: filter}, this.filter);
-    }
-  }
-
   render() {
     return (
       <div className="App">
-        <NavBar className='not-on-mobile'/>
-        <FilterSideBar toggleFilter={this.handleToggleFilter}/>
-        <div className = 'body'>
-          <GameList games={this.state.games}/>
-        </div>
+        <HomePage/>
       </div>
     );
   }
---
+++ src/Components/HomePage/HomePage.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import NavBar from '../NavBar/NavBar';
+import FilterSideBar from '../FilterSidebar/FilterSidebar';
+import GameList from '../GameList/GameList';
+import GameData from '../../mockGameData';
+import './homepage.less';
+
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      games: GameData,
+      genreFilters: [],
+      platformFilters: [],
+      ratingFilter: 0,
+      textFilter: ''
+    }
+
+    this.filter = this.filter.bind(this);
+    this.handleToggleFilter = this.handleToggleFilter.bind(this);
+  }
+
+  filter() {
+    let games = GameData;
+    games = games.filter((game) => {
+      let genreFilter = true;
+      let platformFilter = true;
+      let ratingFilter = true;
+      let textFilter = true;
+
+      this.state.genreFilters.forEach((genreId) => {
+        genreFilter = genreFilter && game.genres.some(genre => genre.id === genreId);
+      })
+
+      this.state.platformFilters.forEach((platformId) => {
+        platformFilter = platformFilter && game.platforms.some(platform => platform.id === platformId);
+      })
+
+      ratingFilter = game.rating >= this.state.ratingFilter;
+
+      textFilter = game.title.toLowerCase().includes(this.state.textFilter.toLowerCase());
+
+      return genreFilter && platformFilter && ratingFilter && textFilter;
+    });
+
+    this.setState({games});
+  }
+
+  addOrRemoveFilter(id, filterList) {
+    let activeFilters = filterList;
+    let activeIndex = activeFilters.indexOf(id);
+
+    if(activeIndex > -1){
+      activeFilters.splice(activeIndex, 1);
+    }
+    else {
+      activeFilters.push(id);
+    }
+
+    return activeFilters;
+  }
+
+  handleToggleFilter(filter, type) {
+    if(type === 0){
+      this.setState({genreFilters: this.addOrRemoveFilter(filter, this.state.genreFilters)}, this.filter);
+    }
+    if(type === 1){
+      this.setState({platformFilters: this.addOrRemoveFilter(filter, this.state.platformFilters)}, this.filter);
+    }
+    if(type === 2) {
+      this.setState({ratingFilter: filter}, this.filter);
+    }
+
+    if(type === 3) {
+      this.setState({textFilter: filter}, this.filter);
+    }
+  }
+
+  render() {
+    return (
+      <div className="home-page">
+        <NavBar className='not-on-mobile'/>
+        <FilterSideBar toggleFilter={this.handleToggleFilter}/>
+        <div className = 'body'>
+          <GameList games={this.state.games}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
