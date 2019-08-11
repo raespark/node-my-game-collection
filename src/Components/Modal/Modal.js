b6833f2008cb390bba6b416f8cd06d024c4b576a
Rachael Metcalf <raespark.games@gmail.com>
2019-08-11 11:27:06 -0500

    add more z-indexes to screenshots to fix hover scale bug

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -7,7 +7,7 @@ class Modal extends Component {
     return (
       <div className={classnames(this.props.className, 'modal')} onClick={this.props.onClick}>
         <div className="modal-container" onClick={this.props.closeModal}>
-          <div className="modal-content" onClick={() => {console.log('CLICK :D')}}>
+          <div className="modal-content">
             {this.props.children}
           </div>
         </div>
